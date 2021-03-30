3dd4c70fb8c9c08b8a0d961c6eb43208e50aa3ca
Zsolt Gulyas <zsolt.gulyas@outlook.com>
2021-03-30 10:04:41 +0200

    Add programmatic web component creation example

---
+++ examples/programmatic-custom-element-creation/scripts.js
@@ -0,0 +1,40 @@
+
+class MyComponent extends HTMLElement {
+    constructor() {
+        const templateElement = document.createElement('template');
+        templateElement.innerHTML = `<ul></ul>`;
+        super() // returns element this scope
+            .attachShadow({mode: 'open'}) // sets AND returns this.shadowRoot
+            .append(templateElement.content.cloneNode(true));
+    }
+    set listItems(items) {
+        this._listItems = items;
+        this._renderLis();
+    }
+    get listItems() {
+        return this._listItems;
+    }
+    _renderLis() {
+        const ul = this.shadowRoot.querySelector('ul');
+        
+        this.listItems.forEach(item => {
+            const li = document.createElement('li');
+            li.innerText = item;
+            ul.appendChild(li);
+        });
+        
+    }
+}
+
+customElements.define('my-component', MyComponent);
+
+// Return a reference to the my-paragraph constructor
+let MyComponentConstructor = customElements.get('my-component');
+
+const myComponentInstance = new MyComponentConstructor();
+myComponentInstance.id = "myId";
+myComponentInstance.listItems = ['Gandalf', 'Legolas', 'Aragorn'];
+
+document.body.appendChild(myComponentInstance);
+
+console.log(myComponentInstance);
